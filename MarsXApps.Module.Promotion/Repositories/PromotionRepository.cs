def84fdf8d576dd802679b90e9c43fe8de6f2c9d
Pichet Ch <>
2025-04-30 10:14:54 +0700

    initial

---
+++ MarsXApps.Module.Promotion/Repositories/PromotionRepository.cs
@@ -0,0 +1,305 @@
+﻿using MarsXApps.Models;
+using MarsXApps.Models.Constants;
+using MarsXApps.Models.Customs;
+using MarsXApps.Models.Customs.MethodType;
+using MarsXApps.Models.Filter.Promotion;
+using MarsXApps.Service;
+using MarsXApps.Service.Models;
+namespace MarsXApps.Module.Promotion.Repositories
+{
+    public interface IPromotionRepository
+    {
+        Task<Response> GetAll(PromotionFilter param);
+        Task<Response> GetById(int id);
+        Task<Response> Create(PromotionModel param);
+        Task<Response> Update(PromotionModel param);
+        Task<Response> GetByCode(string Code);
+        Task<Response> CreatePromotionWithWidget(PromotionModel param);
+        Task<Response> UpdatePromotionWithWidget(PromotionModel param);
+    }
+    public class PromotionRepository : IPromotionRepository
+    {
+        DateTime ServerTime;
+
+        private readonly Mapper _mapper;
+
+        private readonly IPromotionService _service;
+
+        public PromotionRepository(IPromotionService service)
+        {
+            _mapper = new Mapper();
+            _service = service;
+
+        }
+
+        public async Task<Response> GetAll(PromotionFilter param)
+        {
+            Response resp = new Response();
+            try
+            {
+              
+                List<PromotionModel> Outbound = await _service.GetAll(param);
+               
+
+                if (Outbound != null)
+                {
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+
+                    resp.PageNumber = param.PageNumber;
+                    resp.PageSize = param.PageSize;
+                    resp.EffectRow = await _service.CountAsync();
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> GetById(int id)
+        {
+            Response resp = new Response();
+            try
+            {
+                PromotionModel Outbound = await _service.GetById(id);
+                if (Outbound != null)
+                {
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> Create(PromotionModel param)
+        {
+            Response resp = new();
+            MarsXApps.Service.Models.Promotion Inbound = new();
+            PromotionModel Outbound = new();
+            try
+            {
+                Inbound = _mapper.Map(param);
+                Inbound.CreateDate = ServerTime;
+                Inbound.UpdateDate = ServerTime;
+
+                bool duplicate = await _service.DuplicateKey(Inbound);
+
+                if (duplicate == false)
+                {
+                    Outbound = await _service.Create(Inbound);
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Output = Constants.InvalidDataDuplicate;
+                }
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> Update(PromotionModel param)
+        {
+            Response resp = new Response();
+            MarsXApps.Service.Models.Promotion Inbound = new();
+            PromotionModel Outbound = new();
+
+            try
+            {
+                Inbound = _mapper.Map(param);
+
+                if (Inbound != null)
+                {
+                    Inbound.UpdateDate = ServerTime;
+
+                    Outbound = await _service.Update(Inbound);
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.UpdateDataNotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> GetByCode(string Code)
+        {
+            Response resp = new Response();
+            try
+            {
+                PromotionModel Outbound = await _service.GetByCode(Code);
+                Outbound.CmsWidgetDisplayPath = Outbound.CmsWidgetModel.DisplayPath;
+                Outbound.CmsWidgetModel = null;
+                if (Outbound != null)
+                {
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> CreatePromotionWithWidget(PromotionModel param)
+        {
+            Response resp = new();
+            MarsXApps.Service.Models.Promotion Inbound = new();
+            PromotionModel Outbound = new();
+            try
+            {
+                Inbound = _mapper.Map(param);
+                Inbound.CmsWidgetNameNavigation = _mapper.Map(param.CmsWidgetModel);
+                Inbound.CreateDate = ServerTime;
+                Inbound.UpdateDate = ServerTime;
+
+                bool duplicate = await _service.DuplicateKey(Inbound);
+
+                if (duplicate == false)
+                {
+                    Outbound = await _service.CreatePromotionWithWidget(Inbound);
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode204;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Output = Constants.InvalidDataDuplicate;
+                }
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+
+        public async Task<Response> UpdatePromotionWithWidget(PromotionModel param)
+        {
+            Response resp = new();
+            MarsXApps.Service.Models.Promotion Inbound = new();
+            PromotionModel Outbound = new();
+            try
+            {
+                Inbound = _mapper.Map(param);
+                Inbound.CmsWidgetNameNavigation = _mapper.Map(param.CmsWidgetModel);
+                bool duplicate = await _service.DuplicateKey(Inbound);
+
+                if (duplicate == false)
+                {
+                    Outbound = await _service.UpdatePromotionWithWidget(Inbound);
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode204;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Output = Constants.InvalidDataDuplicate;
+                }
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+    }
+}
---
+++ MarsXApps.Module.ServicePrice/Repositories/ServicePriceRepository.cs
@@ -0,0 +1,185 @@
+﻿using MarsXApps.Models;
+using MarsXApps.Models.Constants;
+using MarsXApps.Models.Customs;
+using MarsXApps.Models.Customs.CAEC;
+using MarsXApps.Models.Filter.ServiceAfterSale;
+using MarsXApps.Service;
+using MarsXApps.Service.Models;
+using MarsXApps.Service.Validate;
+using Microsoft.Extensions.Configuration;
+
+namespace MarsXApps.Module.ServicePrice.Repositories
+{
+    public interface IServicePriceRepository
+    {
+        public Task<Response> GetPMPrice(string mcModel);
+        public Task<Response> GetPMPriceModels();
+        public Task<Response> GetPMPriceHour(string mcModel, int pmHour);
+    }
+
+    public class ServicePriceRepository : IServicePriceRepository
+    {
+        DateTime ServerTime;
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+
+        private readonly Mapper _mapper;
+
+
+        private readonly IServicePriceService _service;
+
+        public ServicePriceRepository(IServicePriceService service)
+        {
+            _service = service;
+
+            _mapper = new Mapper();
+
+
+            ServerTime = DateTime.Now.MarsX();
+
+        }
+
+        public async Task<Response> GetPMPriceModels()
+        {
+            Response resp = new Response();
+
+            List<SysCustomerMachineModelAll> Outbound = new List<SysCustomerMachineModelAll>();
+
+            try
+            {
+                var respModels = await _service.GetPMPriceModels();
+
+                if (respModels.Count > 0)
+                {
+                    foreach (var item in respModels)
+                    {
+                        SysCustomerMachineModelAll obj = new SysCustomerMachineModelAll();
+
+                        obj.Key = item;
+                        obj.Name = item;
+
+                        Outbound.Add(obj);
+                    }
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode204;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        } 
+        public async Task<Response> GetPMPrice(string mcModel)
+        {
+            Response resp = new Response();
+
+            List<MachinePMModel> Outbound = new List<MachinePMModel>();
+
+            try
+            {
+                Outbound = await _service.GetPMPrice(mcModel);
+
+                if (Outbound.Count > 0)
+                {
+                    Outbound = (from t in Outbound
+                                group t by new { t.mcModelGobal, t.pmHour }
+                                into grp
+                                select new MachinePMModel
+                                {
+                                    mcModelGobal = grp.Key.mcModelGobal,
+                                    pmHour = grp.Key.pmHour,
+                                    noOfItems = grp.Count(),
+                                    uiKeyValue = $"{grp.Key.mcModelGobal}|{grp.Key.pmHour}",
+                                    sumPrice = grp.Sum(s => Convert.ToDecimal(s.unitPrice)).ToString()
+
+                                }).OrderBy(o => o.pmHour)
+                     .ToList();
+
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+        public async Task<Response> GetPMPriceHour(string mcModel, int pmHour)
+        {
+            Response resp = new Response();
+
+            List<MachinePMModel> Outbound = new List<MachinePMModel>();
+
+            try
+            {
+                Outbound = await _service.GetPMPrice(mcModel);
+
+                if (Outbound.Count > 0)
+                {
+                    Outbound = Outbound.Where(w => w.pmHour == pmHour).OrderBy(o=>o.seqNo).ToList();
+                    var sumPrice = Outbound.Sum(s => Convert.ToDecimal(s.unitPrice)).ToString();
+                    Outbound.ForEach(f => f.sumPrice = sumPrice);
+                    resp.Status = Constants.StatusSuccess;
+                    resp.HttpCode = Constants.HttpCode200;
+                    resp.HttpMessage = Constants.HttpCode200Message;
+                    resp.Output = Outbound;
+                }
+                else
+                {
+                    resp.Status = Constants.StatusError;
+                    resp.HttpCode = Constants.HttpCode400;
+                    resp.HttpMessage = Constants.HttpCode204Message;
+                    resp.Message = Constants.RecordDataNotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resp.Status = Constants.StatusError;
+                resp.HttpCode = Constants.HttpCode500;
+                resp.Message = Constants.HttpCode500Message;
+                resp.InnerException = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+            }
+
+            return resp;
+        }
+    }
+}
+
+
+
+
